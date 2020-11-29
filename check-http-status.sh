31ad4f296bcbcea548096c59b2532c697bfb74a6
inabnitn <othersortsofthings@gmail.com>
2020-11-29 12:28:51 -1000

    Switched to parallel processing instead of sequential.

--- check-http-status.sh
+++ check-http-status.sh
@@ -10,11 +10,11 @@
 #
 ###############################################################################################
 
-
 # Number of seconds curl should try before giving up.
 timeout=5
 
 # The file containing the list of URLs to check.
+#input_file=list-of-urls
 input_file=list-of-urls
 
 # Make sure we have read access to the input file.
@@ -29,6 +29,12 @@ if [[ ! -s "$input_file" ]] ; then
    exit 1
 fi
 
+# Use a hidden file to track if any URL failed.
+failed_flag=".failed_flag"
+
+# Remove the flag file if it exists from the previous run.
+rm -f "$failed_flag"
+
 # Define terminal text colors, for later use.
 # More info can be found in the 'Color Handling' section of the man page
 # for 'terminfo'.
@@ -36,42 +42,47 @@ red=$(tput setaf 1)
 green=$(tput setaf 2)
 color_off=$(tput sgr0)
 
-# Flag if any URL fails.
-failed=0
-
-# Read in the list of URLs, one line at a time.
-while read url ; do
-
+function get_http_status_code {
    # First check if curl can even reach the destination. There could be situations
-   # where curl doesn't receive any HTTP status code, such as with malformed URLs.
-   curl --max-time "$timeout" --output /dev/null --silent "$url"
+   # where curl doesn't receive any HTTP status code, such as with bad URLs.
+   curl --max-time "$timeout" --output /dev/null --silent "$1"
+   
    if [[ $? -ne 0 ]] ; then
-      echo "${red}FAILED - *** - ${color_off}no HTTP status code received, because curl could not reach ${url}"
-      echo
+      echo "${red}FAILED - *** - ${color_off}no HTTP status code received, because curl could not reach $1"
       failed=1
       continue
    fi
    
    # If the check above passed, get the HTTP status code for the URL.
-   # In the curl command below, the '--location' option means that curl will try the URL's new
-   # location if the web server reports that the page has moved, such as with a 301 status code.
-   http_status=$(curl --max-time "$timeout" --location --silent --head --output /dev/null --write-out "%{http_code}" "$url")
-
+   # The curl command below is set to follow redirects. Usually this works fine, but for some web sites this
+   # can result in unexpected behavior. Remove the '--location' option if you don't want curl to follow redirects.
+   http_status=$(curl --max-time "$timeout" --location --silent --output /dev/null --write-out "%{http_code}" "$1")
+   
    # Color the results based on the HTTP status code.
    # Status codes 4xx and 5xx are bad; everything else is OK.
    # More info at https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
    if [[ "$http_status" == [45][0-9][0-9] ]] ; then
-      echo "${red}FAILED - ${http_status} - ${color_off}$url"
+      echo "${red}FAILED - ${http_status} - ${color_off}$1"
       failed=1
    else
       # Add extra white space so that all results (failed and succeeded) line up nicely.
-      echo "    ${green}OK - ${http_status} - ${color_off}$url"
+      echo "    ${green}OK - ${http_status} - ${color_off}$1"
    fi
-   echo
+}
 
+# Read in the list of URLs from the input file.
+while read url ; do
+   # Call each instance of 'get_http_status_code' as a separate background process,
+   # so that essentially all URLs are checked in parallel, rather than one at a time.
+   # This works well unless you are checking hundreds of URLs at once. In that case,
+   # remove the ampersand below so that each URL is checked one at a time.
+   get_http_status_code "$url" &
 done < "$input_file"
 
+# Wait for all background processes to finish.
+wait
+
 # Exit with non-zero status if any URL failed.
-if (( "$failed" )) ; then
+if [[ -e "$failed_flag" ]] ; then
    exit 1
 fi
